b96bbd0148b09911973777c709040b1cd2f0f3a0
Natanael Bezerra <nsbbezerra@hotmail.com>
2022-05-26 10:29:02 -0300

    finalize employees components and methods

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const configs = {
   url: "http://localhost:4003",
+  pagination: 10,
 };
 
 const api = axios.create({
--- src/pages/employees/list.tsx
+++ src/pages/employees/list.tsx
@@ -36,25 +36,27 @@ import {
   PopoverFooter,
   PopoverArrow,
   PopoverCloseButton,
-  PopoverAnchor,
   RadioGroup,
   Radio,
+  Grid,
+  FormControl,
+  FormLabel,
 } from "@chakra-ui/react";
 import axios from "axios";
-import { Fragment, useEffect, useRef, useState } from "react";
+import { Fragment, useEffect, useState, useRef } from "react";
 import { useQuery, QueryClient, useMutation } from "react-query";
-import { api } from "../../configs";
+import { api, configs } from "../../configs";
 import { GiCardboardBox } from "react-icons/gi";
 import {
   AiOutlineEdit,
-  AiOutlineKey,
   AiOutlineTool,
   AiOutlineClose,
   AiOutlineSave,
 } from "react-icons/ai";
-
+import * as Yup from "yup";
+import Input from "../../components/Input";
 import { Form } from "@unform/web";
-import { SubmitHandler, FormHandles } from "@unform/core";
+import { FormHandles, SubmitHandler } from "@unform/core";
 
 type EmployeeProps = {
   id: string;
@@ -64,6 +66,13 @@ type EmployeeProps = {
   password: string;
   permission: "all" | "cashier" | "seller";
   active: boolean;
+};
+
+type EmployeeUpdateProps = {
+  name: string;
+  phone: string;
+  user: string;
+  password: string;
 };
 
 type UserProps = {
@@ -84,17 +93,38 @@ type AuthProps = {
   active: boolean;
 };
 
+type AuthUpdateProps = {
+  user: string;
+  password: string;
+};
+
 const queryClient = new QueryClient();
 
 const ListEmployee = () => {
   const toast = useToast();
-  const formRefPermission = useRef<FormHandles>(null);
+  const formRef = useRef<FormHandles>(null);
   const [user, setUser] = useState<UserProps>();
+  const [page, setPage] = useState<number>(0);
+  const [pages, setPages] = useState<number>(0);
   const [employees, setEmployees] = useState<EmployeeProps[]>([]);
+  const [employeesAll, setEmployeesAll] = useState<EmployeeProps[]>([]);
   const [company, setCompany] = useState<CompanyProps>();
   const [modalInfo, setModalInfo] = useState<boolean>(false);
-  const [modalAuth, setModalAuth] = useState<boolean>(false);
   const [permission, setPermission] = useState<string>();
+  const [employee, setEmployee] = useState<EmployeeProps>();
+
+  const paginateGood = (array: EmployeeProps[], page_size: number) => {
+    const pag = array.slice(page * page_size, page * page_size + page_size);
+    setEmployees(pag);
+  };
+
+  useEffect(() => {
+    paginateGood(employeesAll, configs.pagination);
+  }, [page]);
+
+  useEffect(() => {
+    setPages(Math.ceil(employeesAll.length / configs.pagination));
+  }, [employeesAll]);
 
   function showToast(
     message: string,
@@ -138,7 +168,8 @@ const ListEmployee = () => {
 
   useEffect(() => {
     if (data) {
-      setEmployees(data);
+      paginateGood(data, configs.pagination);
+      setEmployeesAll(data);
     }
   }, [data]);
 
@@ -200,6 +231,60 @@ const ListEmployee = () => {
       },
     }
   );
+
+  const mutationAuth = useMutation(
+    (data: AuthUpdateProps) => {
+      console.log(data);
+      return api.put(`/changeAuthInfo/${employee?.id}`, data, {
+        headers: { "x-access-authorization": user?.token || "" },
+      });
+    },
+    {
+      onSuccess: async (response) => {
+        showToast(response.data.message, "success", "Sucesso");
+        queryClient.invalidateQueries("employees");
+        setModalInfo(false);
+      },
+      onError: (err) => {
+        if (axios.isAxiosError(error) && error.message) {
+          showToast(error.response?.data.message, "error", "Erro");
+        }
+      },
+    }
+  );
+
+  const handleUpdateInfo: SubmitHandler<EmployeeUpdateProps> = async (data) => {
+    try {
+      const schema = Yup.object().shape({
+        user: Yup.string().required("Insira um usuário"),
+        password: Yup.string().required("Insira  uma senha"),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+
+      mutationAuth.mutate({
+        user: data.user,
+        password: data.password,
+      });
+    } catch (error) {
+      if (error instanceof Yup.ValidationError) {
+        error.inner.forEach((err) => {
+          showToast(err.message, "error", "Erro");
+        });
+      } else {
+        let message = (error as Error).message;
+        showToast(message, "error", "Erro");
+      }
+    }
+  };
+
+  const handeToUpdate = (emp: string) => {
+    const result = employees.find((obj) => obj.id === emp);
+    setEmployee(result);
+    setModalInfo(true);
+  };
 
   return (
     <Fragment>
@@ -340,12 +425,9 @@ const ListEmployee = () => {
                             <MenuList>
                               <MenuItem
                                 icon={<AiOutlineEdit />}
-                                onClick={() => setModalInfo(true)}
+                                onClick={() => handeToUpdate(emp.id)}
                               >
                                 Alterar Informações
-                              </MenuItem>
-                              <MenuItem icon={<AiOutlineKey />}>
-                                Alterar Login
                               </MenuItem>
                             </MenuList>
                           </Menu>
@@ -355,30 +437,77 @@ const ListEmployee = () => {
                   </Tbody>
                 </Table>
               )}
+              <Flex justify={"flex-end"} align="center" gap={3} mt={3}>
+                <Button
+                  size={"sm"}
+                  onClick={() => setPage(page - 1)}
+                  isDisabled={page + 1 === 1}
+                >
+                  Anterior
+                </Button>
+                <Text>
+                  {page + 1} / {pages}
+                </Text>
+                <Button
+                  size={"sm"}
+                  onClick={() => setPage(page + 1)}
+                  isDisabled={page + 1 === pages}
+                >
+                  Próxima
+                </Button>
+              </Flex>
             </Fragment>
           )}
         </Box>
       </Box>
 
-      <Modal isOpen={modalInfo} onClose={() => setModalInfo(false)}>
+      <Modal isOpen={modalInfo} onClose={() => setModalInfo(false)} size="3xl">
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>Informações do Colaborador</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody></ModalBody>
+          <Form
+            ref={formRef}
+            onSubmit={handleUpdateInfo}
+            initialData={{ user: employee?.user }}
+          >
+            <ModalHeader>Informações do Colaborador</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <Stack spacing={3}>
+                <Grid templateColumns={"1fr 1fr"} gap={3}>
+                  <FormControl isRequired>
+                    <FormLabel>Usuário</FormLabel>
+                    <Input placeholder="Usuário" name="user" autoFocus />
+                  </FormControl>
+                  <FormControl isRequired>
+                    <FormLabel>Senha</FormLabel>
+                    <Input
+                      placeholder="Senha"
+                      name="password"
+                      type="password"
+                    />
+                  </FormControl>
+                </Grid>
+              </Stack>
+            </ModalBody>
 
-          <ModalFooter>
-            <Button
-              mr={3}
-              onClick={() => setModalInfo(false)}
-              leftIcon={<AiOutlineClose />}
-            >
-              Fechar
-            </Button>
-            <Button colorScheme="blue" leftIcon={<AiOutlineSave />}>
-              Salvar
-            </Button>
-          </ModalFooter>
+            <ModalFooter>
+              <Button
+                mr={3}
+                onClick={() => setModalInfo(false)}
+                leftIcon={<AiOutlineClose />}
+              >
+                Fechar
+              </Button>
+              <Button
+                colorScheme="blue"
+                leftIcon={<AiOutlineSave />}
+                type="submit"
+                isLoading={mutationAuth.isLoading}
+              >
+                Salvar
+              </Button>
+            </ModalFooter>
+          </Form>
         </ModalContent>
       </Modal>
     </Fragment>
