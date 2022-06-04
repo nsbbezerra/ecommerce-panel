7a7f1106f928ce6c6b17d756b67b7a6282c50110
Natanael Bezerra <nsbbezerra@hotmail.com>
2022-06-04 17:16:38 -0300

    add taxes fields to register product component

---
+++ src/configs/data.ts
@@ -0,0 +1,136 @@
+const dataTrib = [
+  {
+    code: "01",
+    desc: "01 - Operação Tributável com Alíquota Básica",
+  },
+  {
+    code: "02",
+    desc: "02 - Operação Tributável com Alíquota Diferenciada",
+  },
+  {
+    code: "03",
+    desc: "03 - Operação Tributável com Alíquota por Unidade de Medida de Produto",
+  },
+  {
+    code: "04",
+    desc: "04 - Operação Tributável Monofásica – Revenda a Alíquota Zero",
+  },
+  {
+    code: "05",
+    desc: "05 - Operação Tributável por Substituição Tributária",
+  },
+  {
+    code: "06",
+    desc: "06 - Operação Tributável a Alíquota Zero",
+  },
+  {
+    code: "07",
+    desc: "07 - Operação Isenta da Contribuição",
+  },
+  {
+    code: "08",
+    desc: "08 - Operação sem Incidência da Contribuição",
+  },
+  {
+    code: "09",
+    desc: "09 - Operação com Suspensão da Contribuição",
+  },
+  {
+    code: "49",
+    desc: "49 - Outras Operações de Saída",
+  },
+  {
+    code: "50",
+    desc: "50 - Operação com Direito a Crédito – Vinculada Exclusivamente a Receita Tributada no Mercado Interno",
+  },
+  {
+    code: "51",
+    desc: "51 - Operação com Direito a Crédito – Vinculada Exclusivamente a Receita Não Tributada no Mercado Interno",
+  },
+  {
+    code: "52",
+    desc: "52 - Operação com Direito a Crédito – Vinculada Exclusivamente a Receita de Exportação",
+  },
+  {
+    code: "53",
+    desc: "53 - Operação com Direito a Crédito – Vinculada a Receitas Tributadas e Não-Tributadas no Mercado Interno",
+  },
+  {
+    code: "54",
+    desc: "54 - Operação com Direito a Crédito – Vinculada a Receitas Tributadas no Mercado Interno e de Exportação",
+  },
+  {
+    code: "55",
+    desc: "55 - Operação com Direito a Crédito – Vinculada a Receitas Não-Tributadas no Mercado Interno e de Exportação",
+  },
+  {
+    code: "56",
+    desc: "56 - Operação com Direito a Crédito – Vinculada a Receitas Tributadas e Não-Tributadas no Mercado Interno, e de Exportação",
+  },
+  {
+    code: "60",
+    desc: "60 - Crédito Presumido – Operação de Aquisição Vinculada Exclusivamente a Receita Tributada no Mercado Interno",
+  },
+  {
+    code: "61",
+    desc: "61 - Crédito Presumido – Operação de Aquisição Vinculada Exclusivamente a Receita Não-Tributada no Mercado Interno",
+  },
+  {
+    code: "62",
+    desc: "62 - Crédito Presumido – Operação de Aquisição Vinculada Exclusivamente a Receita de Exportação",
+  },
+  {
+    code: "63",
+    desc: "63 - Crédito Presumido – Operação de Aquisição Vinculada a Receitas Tributadas e Não-Tributadas no Mercado Interno",
+  },
+  {
+    code: "64",
+    desc: "64 - Crédito Presumido – Operação de Aquisição Vinculada a Receitas Tributadas no Mercado Interno e de Exportação",
+  },
+  {
+    code: "65",
+    desc: "65 - Crédito Presumido – Operação de Aquisição Vinculada a Receitas Não-Tributadas no Mercado Interno e de Exportação",
+  },
+  {
+    code: "66",
+    desc: "66 - Crédito Presumido – Operação de Aquisição Vinculada a Receitas Tributadas e Não-Tributadas no Mercado Interno, e de Exportação",
+  },
+  {
+    code: "67",
+    desc: "67 - Crédito Presumido – Outras Operações",
+  },
+  {
+    code: "70",
+    desc: "70 - Operação de Aquisição sem Direito a Crédito",
+  },
+  {
+    code: "71",
+    desc: "71 - Operação de Aquisição com Isenção",
+  },
+  {
+    code: "72",
+    desc: "72 - Operação de Aquisição com Suspensão",
+  },
+  {
+    code: "73",
+    desc: "73 - Operação de Aquisição a Alíquota Zero",
+  },
+  {
+    code: "74",
+    desc: "74 - Operação de Aquisição sem Incidência da Contribuição",
+  },
+  {
+    code: "75",
+    desc: "75 - Operação de Aquisição por Substituição Tributária",
+  },
+  {
+    code: "98",
+    desc: "98 - Outras Operações de Entrada",
+  },
+  {
+    code: "99",
+    desc: "99 - Outras Operações",
+  },
+];
+
+export { dataTrib };
--- src/pages/products/register.tsx
+++ src/pages/products/register.tsx
@@ -25,6 +25,8 @@ import {
   useToast,
   useColorModeValue,
   theme,
+  Select as ChakraSelect,
+  Tooltip,
 } from "@chakra-ui/react";
 import { SubmitHandler, FormHandles } from "@unform/core";
 import { Form } from "@unform/web";
@@ -36,6 +38,7 @@ import { AiOutlinePlus } from "react-icons/ai";
 import { FaRuler, FaTrashAlt } from "react-icons/fa";
 import RichTextEditor from "react-rte";
 import { CgArrowRight } from "react-icons/cg";
+import { dataTrib } from "../../configs/data";
 
 type CostValueProps = {
   title: string;
@@ -458,7 +461,240 @@ const RegisterProduct = () => {
               </Stack>
             </TabPanel>
             <TabPanel>
-              <p>two!</p>
+              <Stack spacing={3}>
+                <Grid templateColumns={"repeat(3, 1fr)"} gap={3}>
+                  <FormControl>
+                    <FormLabel>CEST</FormLabel>
+                    <ChakraInput placeholder="CEST" />
+                  </FormControl>
+                  <FormControl>
+                    <FormLabel>NCM</FormLabel>
+                    <ChakraInput placeholder="NCM" />
+                  </FormControl>
+                  <FormControl>
+                    <FormLabel>CFOP</FormLabel>
+                    <ChakraInput placeholder="CFOP" />
+                  </FormControl>
+                </Grid>
+                <Grid templateColumns={"repeat(4, 1fr)"} gap={3}>
+                  <FormControl>
+                    <FormLabel>ICMS Origem</FormLabel>
+                    <ChakraSelect placeholder="Selecione uma opção">
+                      <option value={"0"}>0 - Nacional</option>
+                      <option value={"1"}>
+                        1 - Estrangeira (importação direta)
+                      </option>
+                      <option value={"2"}>
+                        2 - Estrangeira (adquirida no mercado interno)
+                      </option>
+                      <option value={"3"}>
+                        3 - Nacional com mais de 40% de conteúdo estrangeiro
+                      </option>
+                      <option value={"4"}>
+                        4 - Nacional produzida através de processos produtivos
+                        básicos
+                      </option>
+                      <option value={"5"}>
+                        5 - Nacional com menos de 40% de conteúdo estrangeiro
+                      </option>
+                      <option value={"6"}>
+                        6 - Estrangeira (importação direta) sem produto nacional
+                        similar
+                      </option>
+                      <option value={"7"}>
+                        7 - Estrangeira (adquirida no mercado interno) sem
+                        produto nacional similar
+                      </option>
+                      <option value={"8"}>
+                        8 - Nacional, mercadoria ou bem com Conteúdo de
+                        Importação superior a 70%;
+                      </option>
+                    </ChakraSelect>
+                  </FormControl>
+                  <FormControl>
+                    <FormLabel>ICMS CSOSN</FormLabel>
+                    <ChakraSelect placeholder="Selecione uma opção">
+                      <option value={"101"}>
+                        101 - Tributada pelo Simples Nacional com permissão de
+                        crédito
+                      </option>
+                      <option value={"102"}>
+                        102 - Tributada pelo Simples Nacional sem permissão de
+                        crédito
+                      </option>
+                      <option value={"103"}>
+                        103 - Isenção do ICMS no Simples Nacional para faixa de
+                        receita bruta
+                      </option>
+                      <option value={"201"}>
+                        201 - Tributada pelo Simples Nacional com permissão de
+                        crédito e com cobrança do ICMS por substituição
+                        tributária
+                      </option>
+                      <option value={"202"}>
+                        202 - Tributada pelo Simples Nacional sem permissão de
+                        crédito e com cobrança do ICMS por substituição
+                        tributária
+                      </option>
+                      <option value={"203"}>
+                        203 - Isenção do ICMS no Simples Nacional para faixa de
+                        receita bruta e com cobrança do ICMS por substituição
+                        tributária
+                      </option>
+                      <option value={"300"}>300 - Imune</option>
+                      <option value={"400"}>
+                        400 - Não tributada pelo Simples Nacional
+                      </option>
+                      <option value={"500"}>
+                        500 - ICMS cobrado anteriormente por substituição
+                        tributária (substituído) ou por antecipação
+                      </option>
+                      <option value={"900"}>900 - Outros</option>
+                    </ChakraSelect>
+                  </FormControl>
+                  <FormControl>
+                    <FormLabel>ICMS Alíquota (%)</FormLabel>
+                    <ChakraInput placeholder="ICMS Alíquota" />
+                  </FormControl>
+                  <FormControl>
+                    <FormLabel>ICMS Base de Cálculo</FormLabel>
+                    <ChakraInput placeholder="ICMS Base de Cálculo" />
+                  </FormControl>
+                </Grid>
+                <Grid templateColumns={"repeat(4, 1fr)"} gap={3}>
+                  <FormControl>
+                    <FormLabel>
+                      <Tooltip label="Margem de valor agregado" hasArrow>
+                        <Text>ICMS MVA (%)</Text>
+                      </Tooltip>
+                      <ChakraInput placeholder="Margem de valor agregado" />
+                    </FormLabel>
+                  </FormControl>
+                  <FormControl>
+                    <FormLabel>ICMS ST Alíquota (%)</FormLabel>
+                    <ChakraInput placeholder="ICMS ST Alíquota (%)" />
+                  </FormControl>
+                  <FormControl>
+                    <FormLabel>ICMS ST Modalidade de Cálculo (%)</FormLabel>
+                    <ChakraSelect placeholder="Selecione uma opção">
+                      <option value={"0"}>
+                        Preço tabelado ou máximo sugerido
+                      </option>
+                      <option value={"1"}>Lista Negativa (valor)</option>
+                      <option value={"2"}>Lista Positiva (valor)</option>
+                      <option value={"3"}>Lista Neutra (valor)</option>
+                      <option value={"4"}>Margem Valor Agregado (%)</option>
+                      <option value={"5"}>Pauta (valor)</option>
+                    </ChakraSelect>
+                  </FormControl>
+                  <FormControl>
+                    <FormLabel>ICMS ST Base de Cálculo</FormLabel>
+                    <ChakraInput placeholder="ICMS ST Base de Cálculo" />
+                  </FormControl>
+                </Grid>
+                <Grid templateColumns={"repeat(5, 1fr)"} gap={3}>
+                  <FormControl>
+                    <FormLabel>FCP Alíquota (%)</FormLabel>
+                    <ChakraInput placeholder="FCP Alíquota (%)" />
+                  </FormControl>
+                  <FormControl>
+                    <FormLabel>FCP ST Alíquota (%)</FormLabel>
+                    <ChakraInput placeholder="FCP ST Alíquota (%)" />
+                  </FormControl>
+                  <FormControl>
+                    <FormLabel>FCP Ret. Alíquota (%)</FormLabel>
+                    <ChakraInput placeholder="FCP Ret. Alíquota (%)" />
+                  </FormControl>
+                  <FormControl>
+                    <FormLabel>FCP Base de Cálculo</FormLabel>
+                    <ChakraInput placeholder="FCP Base de Cálculo" />
+                  </FormControl>
+                  <FormControl>
+                    <FormLabel>FCP ST Base de Cálculo</FormLabel>
+                    <ChakraInput placeholder="FCP ST Base de Cálculo" />
+                  </FormControl>
+                </Grid>
+                <Grid templateColumns={"repeat(3, 1fr)"} gap={3}>
+                  <FormControl>
+                    <FormLabel>IPI CST</FormLabel>
+                    <ChakraSelect placeholder="Selecione uma opção">
+                      <option value={""}>Nenhum</option>
+                      <option value={"00"}>
+                        00 – Entrada com Recuperação de Crédito
+                      </option>
+                      <option value={"01"}>
+                        01 – Entrada Tributada com Alíquota Zero
+                      </option>
+                      <option value={"02"}>02 – Entrada Isenta</option>
+                      <option value={"03"}>03 – Entrada Não Tributada</option>
+                      <option value={"04"}>04 – Entrada Imune</option>
+                      <option value={"05"}>05 – Entrada com Suspensão</option>
+                      <option value={"49"}>49 – Outras Entradas</option>
+                      <option value={"50"}>50 – Saída Tributada</option>
+                      <option value={"51"}>
+                        51 – Saída Tributável com Alíquota Zero
+                      </option>
+                      <option value={"52"}>52 – Saída Isenta</option>
+                      <option value={"53"}>53 – Saída Não Tributada</option>
+                      <option value={"54"}>54 – Saída Imune</option>
+                      <option value={"55"}>55 – Saída com Suspensão</option>
+                      <option value={"99"}>99 – Outras Saídas</option>
+                    </ChakraSelect>
+                  </FormControl>
+                  <FormControl>
+                    <FormLabel>IPI Alíquota (%)</FormLabel>
+                    <ChakraInput placeholder="IPI Alíquota (%)" />
+                  </FormControl>
+                  <FormControl>
+                    <FormLabel>IPI Código</FormLabel>
+                    <ChakraInput placeholder="IPI Código" />
+                  </FormControl>
+                </Grid>
+                <Grid templateColumns={"repeat(3, 1fr)"} gap={3}>
+                  <FormControl>
+                    <FormLabel>PIS CST</FormLabel>
+                    <ChakraSelect placeholder="Selecione uma opção">
+                      {dataTrib.map((dt) => (
+                        <option key={dt.code}>{dt.desc}</option>
+                      ))}
+                    </ChakraSelect>
+                  </FormControl>
+                  <FormControl>
+                    <FormLabel>PIS Alíquota (%)</FormLabel>
+                    <ChakraInput placeholder="PIS Alíquota (%)" />
+                  </FormControl>
+                  <FormControl>
+                    <FormLabel>PIS Base de Cálculo</FormLabel>
+                    <ChakraInput placeholder="PIS Base de Cálculo" />
+                  </FormControl>
+                </Grid>
+                <Grid templateColumns={"repeat(3, 1fr)"} gap={3}>
+                  <FormControl>
+                    <FormLabel>COFINS CST</FormLabel>
+                    <ChakraSelect placeholder="Selecione uma opção">
+                      {dataTrib.map((dt) => (
+                        <option key={dt.code}>{dt.desc}</option>
+                      ))}
+                    </ChakraSelect>
+                  </FormControl>
+                  <FormControl>
+                    <FormLabel>COFINS Alíquota (%)</FormLabel>
+                    <ChakraInput placeholder="COFINS Alíquota (%)" />
+                  </FormControl>
+                  <FormControl>
+                    <FormLabel>COFINS Base de Cálculo</FormLabel>
+                    <ChakraInput placeholder="COFINS Base de Cálculo" />
+                  </FormControl>
+                </Grid>
+                <Button
+                  rightIcon={<CgArrowRight />}
+                  isFullWidth={false}
+                  w="fit-content"
+                  onClick={() => setIndex(2)}
+                >
+                  Próximo
+                </Button>
+              </Stack>
             </TabPanel>
             <TabPanel>
               <p>three!</p>
