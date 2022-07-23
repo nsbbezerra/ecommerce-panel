2e6b949f13b4cb0253c8f179deecc7e990478104
Natanael Bezerra <nsbbezerra@hotmail.com>
2022-07-23 18:53:34 -0300

    init pdv integration with api

--- src/pages/pdv/pdv.tsx
+++ src/pages/pdv/pdv.tsx
@@ -35,11 +35,23 @@ import {
   Tooltip,
   Image,
   Tag,
+  useToast,
+  ToastPositionWithLogical,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Icon,
+  Stack,
 } from "@chakra-ui/react";
-import { Fragment } from "react";
+import { Fragment, useEffect, useState } from "react";
 import {
   AiOutlineBarcode,
   AiOutlineCheck,
+  AiOutlineEnter,
   AiOutlineFilter,
   AiOutlineMore,
   AiOutlinePercentage,
@@ -54,9 +66,119 @@ import { BiRename } from "react-icons/bi";
 import { BsPrinter } from "react-icons/bs";
 import { FaTrashAlt } from "react-icons/fa";
 import Scrollbars from "react-custom-scrollbars";
+import { api, configs } from "../../configs";
+import axios from "axios";
+import { TiPhoneOutline } from "react-icons/ti";
+
+type ClientsProps = {
+  id: string;
+  name: string;
+  cpf: string;
+  phone: string;
+  street: string;
+  number: string;
+  comp: string;
+  district: string;
+  zip_code: string;
+  state: string;
+  city: string;
+};
+
+type ProductsProps = {
+  id: string;
+  title: string;
+  thumbnail: string;
+  barcode: string;
+  sku: string;
+  in_promotion: boolean;
+  sale_value: string;
+  profit_percent: number;
+  unit_desc: string;
+  type_unit:
+    | "square_meter"
+    | "meter"
+    | "unity"
+    | "weight"
+    | "liter"
+    | "without"
+    | "sizes";
+  have_adictional: boolean;
+  type_sale: "unique" | "partition";
+  sub_category: CatProps;
+  category: CatProps;
+};
+
+type CatProps = {
+  title: string;
+};
+
+type Props = {
+  id: string;
+  token: string;
+};
 
 export default function PDV() {
+  const toast = useToast();
   const { colorMode } = useColorMode();
+
+  const [clients, setClients] = useState<ClientsProps[]>();
+  const [client, setClient] = useState<ClientsProps>();
+  const [products, setProducts] = useState<ProductsProps[]>();
+  const [modalClients, setModalClients] = useState<boolean>(false);
+
+  const [auth, setAuth] = useState<Props>();
+
+  function showToast(
+    message: string,
+    status: "error" | "info" | "warning" | "success" | undefined,
+    title: string
+  ) {
+    toast({
+      title: title,
+      description: message,
+      status: status,
+      position: configs.toastPosition as ToastPositionWithLogical,
+      duration: 8000,
+      isClosable: true,
+    });
+  }
+
+  const findClients = async (id: string) => {
+    try {
+      const response = await api.get(`/pdv_clients/${id}`);
+      setClients(response.data);
+    } catch (error) {
+      if (axios.isAxiosError(error) && error.message) {
+        showToast(error.response?.data.message, "error", "Erro");
+      } else {
+        let message = (error as Error).message;
+        showToast(message, "error", "Erro");
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (auth) {
+      findClients(auth?.id || "");
+    }
+  }, [auth]);
+
+  useEffect(() => {
+    const company = localStorage.getItem("company");
+    const userToken = sessionStorage.getItem("user");
+    let companyParse = JSON.parse(company || "");
+    let userParse = JSON.parse(userToken || "");
+    if (companyParse && userParse) {
+      setAuth({ id: companyParse.id, token: userParse.token });
+    } else {
+      showToast(
+        "Informações da empresa e do usuário ausentes",
+        "warning",
+        "Atenção"
+      );
+    }
+  }, []);
+
   const TableRow = () => (
     <Tr>
       <Td>
@@ -150,6 +272,12 @@ export default function PDV() {
     </Box>
   );
 
+  function handleClient(id: string) {
+    const result = clients?.find((obj) => obj.id === id);
+    setClient(result);
+    setModalClients(false);
+  }
+
   return (
     <Fragment>
       <Box py={2} h="full" maxH={"full"} position="relative">
@@ -168,10 +296,90 @@ export default function PDV() {
                 pointerEvents="none"
                 children={<AiOutlineUser />}
               />
-              <Input placeholder="Cliente" />
+              <Input
+                placeholder="Cliente"
+                value={client?.name || ""}
+                isReadOnly
+              />
             </InputGroup>
-            <IconButton aria-label="Detalhes" icon={<AiOutlineMore />} />
-            <Button colorScheme="blue" leftIcon={<AiOutlineSearch />} px={5}>
+
+            <Popover>
+              <PopoverTrigger>
+                <IconButton aria-label="Detalhes" icon={<AiOutlineMore />} />
+              </PopoverTrigger>
+              <PopoverContent shadow="lg" _focus={{ outline: "none" }}>
+                <PopoverArrow />
+                <PopoverCloseButton />
+                <PopoverHeader>Informações</PopoverHeader>
+                <PopoverBody>
+                  <Stack>
+                    <Box rounded="md" borderWidth={"1px"} overflow="hidden">
+                      <Box
+                        bg={useColorModeValue(
+                          "blackAlpha.100",
+                          "whiteAlpha.100"
+                        )}
+                        px={3}
+                        py={1}
+                        fontSize="sm"
+                        fontWeight={"semibold"}
+                      >
+                        CPF
+                      </Box>
+                      <Box p={2} fontSize="sm">
+                        {client?.cpf || ""}
+                      </Box>
+                    </Box>
+                    <Box rounded="md" borderWidth={"1px"} overflow="hidden">
+                      <Box
+                        bg={useColorModeValue(
+                          "blackAlpha.100",
+                          "whiteAlpha.100"
+                        )}
+                        px={3}
+                        py={1}
+                        fontSize="sm"
+                        fontWeight={"semibold"}
+                      >
+                        Contato
+                      </Box>
+                      <Box p={2} fontSize="sm">
+                        {client?.phone || ""}
+                      </Box>
+                    </Box>
+                    <Box rounded="md" borderWidth={"1px"} overflow="hidden">
+                      <Box
+                        bg={useColorModeValue(
+                          "blackAlpha.100",
+                          "whiteAlpha.100"
+                        )}
+                        px={3}
+                        py={1}
+                        fontSize="sm"
+                        fontWeight={"semibold"}
+                      >
+                        Endereço
+                      </Box>
+                      <Box p={2} fontSize="sm">
+                        {!client
+                          ? ""
+                          : `${client?.street || ""}, ${
+                              client?.number || ""
+                            }, ${client?.district || ""}, ${
+                              client?.city || ""
+                            } - ${client?.state || ""}`}
+                      </Box>
+                    </Box>
+                  </Stack>
+                </PopoverBody>
+              </PopoverContent>
+            </Popover>
+            <Button
+              colorScheme="blue"
+              leftIcon={<AiOutlineSearch />}
+              px={5}
+              onClick={() => setModalClients(true)}
+            >
               Buscar
             </Button>
           </HStack>
@@ -235,15 +443,6 @@ export default function PDV() {
                       </Tr>
                     </Thead>
                     <Tbody>
-                      <TableRow />
-                      <TableRow />
-                      <TableRow />
-                      <TableRow />
-                      <TableRow />
-                      <TableRow />
-                      <TableRow />
-                      <TableRow />
-                      <TableRow />
                       <TableRow />
                     </Tbody>
                   </Table>
@@ -315,17 +514,6 @@ export default function PDV() {
                 <Scrollbars autoHide>
                   <Grid templateColumns={"repeat(4, 1fr)"} gap={2}>
                     <ProductShow />
-                    <ProductShow />
-                    <ProductShow />
-                    <ProductShow />
-                    <ProductShow />
-                    <ProductShow />
-                    <ProductShow />
-                    <ProductShow />
-                    <ProductShow />
-                    <ProductShow />
-                    <ProductShow />
-                    <ProductShow />
                   </Grid>
                 </Scrollbars>
               </Box>
@@ -386,6 +574,74 @@ export default function PDV() {
           </Grid>
         </Box>
       </Box>
+
+      <Modal
+        isOpen={modalClients}
+        onClose={() => setModalClients(false)}
+        size="xl"
+        scrollBehavior="inside"
+      >
+        <ModalOverlay />
+        <ModalContent position={"relative"}>
+          <ModalBody>
+            <Box py={2}>
+              <InputGroup size={"lg"}>
+                <InputLeftElement
+                  pointerEvents="none"
+                  children={
+                    <Icon
+                      as={AiOutlineSearch}
+                      color={useColorModeValue("blue.500", "blue.300")}
+                      fontSize="3xl"
+                    />
+                  }
+                />
+                <Input
+                  placeholder="Digite para buscar"
+                  variant={"flushed"}
+                  autoFocus
+                />
+              </InputGroup>
+            </Box>
+
+            <Stack spacing={2} role="listbox" pb={2} mt={2}>
+              {clients?.map((cli) => (
+                <Button
+                  key={cli.id}
+                  leftIcon={<AiOutlineUser fontSize={"20px"} />}
+                  isFullWidth
+                  py={7}
+                  size="lg"
+                  justifyContent={"space-between"}
+                  rightIcon={<AiOutlineEnter />}
+                  textAlign="justify"
+                  _focus={{
+                    bg: useColorModeValue("blue.500", "blue.300"),
+                    color: useColorModeValue("white", "gray.800"),
+                  }}
+                  _hover={{
+                    bg: useColorModeValue("blue.500", "blue.300"),
+                    color: useColorModeValue("white", "gray.800"),
+                  }}
+                  _active={{
+                    bg: useColorModeValue("blue.600", "blue.400"),
+                    color: useColorModeValue("white", "gray.800"),
+                  }}
+                  onClick={() => handleClient(cli.id)}
+                >
+                  <Box w="full" ml={5}>
+                    <Text fontWeight={"medium"}>{cli.name}</Text>
+                    <HStack spacing={5} fontWeight="light" fontSize={"xs"}>
+                      <Text>{cli.phone}</Text>
+                      <Text>{cli.cpf}</Text>
+                    </HStack>
+                  </Box>
+                </Button>
+              ))}
+            </Stack>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
     </Fragment>
   );
 }
